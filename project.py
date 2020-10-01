15029e7a1f24c31ef96b4961c1b51274f49dcb7f
Sarthak Jain <sarthakjain7@outlook.com>
2020-10-01 14:20:34 +0530

    Added working in the form of comments

--- project.py
+++ project.py
@@ -1,8 +1,14 @@
+''' working : This program will automatically display the notifications 
+    of the IPL match on your desktop after a short duration'''
 
+# Gather live up-to-date sports scores.    
 import sports
 
+# Module for displaying desktop notificatioms
 from pynotifier import Notification
 
+# Fetching information about cricket 
 information=sports.get_sport("CRICKET")
-
+    
+# Getting notification using fetched information
 Notification(title="IPL Live Score Updates",description=str(information),duration=60).send()
